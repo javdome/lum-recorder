d2cd5a9e2869f49e4dbbcf0599a6f83b7ecfe248
Javi Domenech <javdome@gmail.com>
2020-12-28 16:34:41 +0100

    Parece que va en electron con modificaciones

---
+++ src/renderer.js
@@ -0,0 +1,128 @@
+const { desktopCapturer } = require('electron')
+
+window.onload = () => {
+    const warningEl = document.getElementById('warning');
+    const videoElement = document.getElementById('videoElement');
+    const captureBtn = document.getElementById('captureBtn');
+    const startBtn = document.getElementById('startBtn');
+    const stopBtn = document.getElementById('stopBtn');
+    const download = document.getElementById('download');
+    const audioToggle = document.getElementById('audioToggle');
+    const micAudioToggle = document.getElementById('micAudioToggle');
+    
+    if('getDisplayMedia' in navigator.mediaDevices) warningEl.style.display = 'none';
+  
+    let blobs;
+    let blob;
+    let rec;
+    let stream;
+    let voiceStream;
+    let desktopStream;
+    
+    const mergeAudioStreams = (desktopStream, voiceStream) => {
+      const context = new AudioContext();
+      const destination = context.createMediaStreamDestination();
+      let hasDesktop = false;
+      let hasVoice = false;
+      if (desktopStream && desktopStream.getAudioTracks().length > 0) {
+        // If you don't want to share Audio from the desktop it should still work with just the voice.
+        const source1 = context.createMediaStreamSource(desktopStream);
+        const desktopGain = context.createGain();
+        desktopGain.gain.value = 0.7;
+        source1.connect(desktopGain).connect(destination);
+        hasDesktop = true;
+      }
+      
+      if (voiceStream && voiceStream.getAudioTracks().length > 0) {
+        const source2 = context.createMediaStreamSource(voiceStream);
+        const voiceGain = context.createGain();
+        voiceGain.gain.value = 0.7;
+        source2.connect(voiceGain).connect(destination);
+        hasVoice = true;
+      }
+        
+      return (hasDesktop || hasVoice) ? destination.stream.getAudioTracks() : [];
+    };
+  
+    captureBtn.onclick = async () => {
+      download.style.display = 'none';
+      const audio = audioToggle.checked || false;
+      const mic = micAudioToggle.checked || false;
+
+
+      // desktopCapturer.getSources({ types: ['window', 'screen'] }).then(async sources => {
+        // for (const source of sources) {
+          // if (source.name === 'Electron') {
+            
+            desktopStream = await navigator.mediaDevices.getUserMedia({
+                audio: false,
+                video: {
+                  mandatory: {
+                    chromeMediaSource: 'desktop',
+                    chromeMediaSourceId: 'screen:0:0',
+                    minWidth: 1280,
+                    maxWidth: 1280,
+                    minHeight: 720,
+                    maxHeight: 720
+                  }
+                }
+              })
+          // }
+        // }
+      // });
+      // desktopStream = await navigator.mediaDevices.getDisplayMedia({ video:true, audio: audio });
+      
+      if (mic === true) {
+        voiceStream = await navigator.mediaDevices.getUserMedia({ video: false, audio: mic });
+      }
+    
+      const tracks = [
+        ...desktopStream.getVideoTracks(), 
+        ...mergeAudioStreams(desktopStream, voiceStream)
+      ];
+      
+      console.log('Tracks to add to stream', tracks);
+      stream = new MediaStream(tracks);
+      console.log('Stream', stream)
+      videoElement.srcObject = stream;
+      videoElement.muted = true;
+        
+      blobs = [];
+    
+      rec = new MediaRecorder(stream, {mimeType: 'video/webm; codecs=vp8,opus'});
+      rec.ondataavailable = (e) => blobs.push(e.data);
+      rec.onstop = async () => {
+        
+        //blobs.push(MediaRecorder.requestData());
+        blob = new Blob(blobs, {type: 'video/webm'});
+        let url = window.URL.createObjectURL(blob);
+        download.href = url;
+        download.download = 'test.webm';
+        download.style.display = 'block';
+      };
+      startBtn.disabled = false;
+      captureBtn.disabled = true;
+      audioToggle.disabled = true;
+      micAudioToggle.disabled = true;
+    };
+  
+    startBtn.onclick = () => {
+      startBtn.disabled = true;
+      stopBtn.disabled = false;
+      rec.start();
+    };
+  
+    stopBtn.onclick = () => {
+      captureBtn.disabled = false;
+      audioToggle.disabled = false;
+      micAudioToggle.disabled = false;
+      startBtn.disabled = true;
+      stopBtn.disabled = true;
+      
+      rec.stop();
+      
+      stream.getTracks().forEach(s=>s.stop())
+      videoElement.srcObject = null
+      stream = null;
+    };
+  };

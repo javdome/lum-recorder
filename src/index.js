ce46533ce61fde61bc062391cf16d32e453b5553
Javi Domenech <javdome@gmail.com>
2021-01-05 08:31:57 +0100

    Fix Bug: Allowing only one instance of electron.

--- src/index.js
+++ src/index.js
@@ -115,45 +115,66 @@ const createWindow = () => {
 
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  
-  globalShortcut.register('Alt+CommandOrControl+1', () => {
-    if(winFace !=null ) {
-      if (bigFace) {
-        winFace.setSize(250,250);
-        bigFace = false;
-        
-      } else {
-        winFace.setSize(640,640);
-        bigFace = true;
-      }
+
+
+
+
+// Allow only one instance of electron
+const gotTheLock = app.requestSingleInstanceLock();
+
+if (!gotTheLock) {
+  app.quit()
+} else {
+  app.on('second-instance', (event, commandLine, workingDirectory) => {
+    // Someone tried to run a second instance, we should focus our window.
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore()
+      mainWindow.focus()
     }
   });
 
-  globalShortcut.register('Alt+CommandOrControl+9', () => {
-    mainWindow.webContents.send('please-stop');
+
+  // This method will be called when Electron has finished
+  // initialization and is ready to create browser windows.
+  // Some APIs can only be used after this event occurs.
+  app.whenReady().then(() => {
+    
+    globalShortcut.register('Alt+CommandOrControl+1', () => {
+      if(winFace !=null ) {
+        if (bigFace) {
+          winFace.setSize(250,250);
+          bigFace = false;
+          
+        } else {
+          winFace.setSize(640,640);
+          bigFace = true;
+        }
+      }
+    });
+
+    globalShortcut.register('Alt+CommandOrControl+9', () => {
+      mainWindow.webContents.send('please-stop');
+    });
+  }).then(createWindow)
+
+  // Quit when all windows are closed, except on macOS. There, it's common
+  // for applications and their menu bar to stay active until the user quits
+  // explicitly with Cmd + Q.
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit();
+    }
   });
-}).then(createWindow)
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
+  app.on('activate', () => {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
 
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
-});
+}
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
